ae2f0f2b54deda08c94b84fb445fce31a8f490b0
Shinta Takagishi <202932018+ibm-Shinta-Takagishi@users.noreply.github.com>
2025-04-14 11:46:34 +0900

    一旦

---
+++ src/lib/type.ts
@@ -0,0 +1,11 @@
+// import type { Generated, Timestamp } from "@/lib/models/types";
+
+// type Cast<Type> = Type extends Generated<Timestamp>
+//   ? Date
+//   : Type extends Generated<infer SelectType>
+//   ? SelectType
+//   : Type;
+
+// export type TS<KyselyType extends Record<string, unknown>> = {
+//   [Key in keyof KyselyType]: Cast<KyselyType[Key]>;
+// };
