9107a2ae6f65e4e954595b6c363b0e36e0087a28
Shinta Takagishi <202932018+ibm-Shinta-Takagishi@users.noreply.github.com>
2025-04-14 15:54:07 +0900

    prisma typedsqlの追加

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,18 @@
-export default function Home() {
-  return <div>ホーム</div>;
+import { allUserSelect } from "@prisma/client/sql";
+import { PrismaClient } from "@prisma/client";
+
+export default async function Home() {
+  const prisma = new PrismaClient();
+  const users = await prisma.$queryRawTyped(allUserSelect());
+
+  return (
+    <div>
+      <h1>投稿一覧</h1>
+      <ul>
+        {users.map((user) => (
+          <li key={user.id}>{user.name}</li>
+        ))}
+      </ul>
+    </div>
+  );
 }
