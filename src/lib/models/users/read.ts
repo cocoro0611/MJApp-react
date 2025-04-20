25e045dcafb7510b6ea076e660633c6e7c4bcb52
Shinta Takagishi <chiroru.0903.0611@icloud.com>
2025-04-20 18:22:34 +0900

    いけない？

--- src/lib/models/users/read.ts
+++ src/lib/models/users/read.ts
@@ -2,8 +2,11 @@
 
 import { db } from "../db";
 import type { UserData } from "./type";
+import { unstable_noStore } from "next/cache";
 
 export const readUsers = async (): Promise<UserData[]> => {
+  unstable_noStore();
+
   const users = await db
     .selectFrom("User")
     .select(["id", "name", "createdAt"])
