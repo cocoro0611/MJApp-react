4fcf0c8836ac94ce44d6ac2402815caee6151dad
Shinta Takagishi <chiroru.0903.0611@icloud.com>
2025-05-03 10:40:17 +0900

    xx

--- src/components/ui/ButtonFixed.tsx
+++ src/components/ui/ButtonFixed.tsx
@@ -6,7 +6,7 @@ interface ButtonFixedProps {
 
 const ButtonFixed = ({ href }: ButtonFixedProps) => {
   return (
-    <Link href={href} className="fixed-btn primary">
+    <Link href={href} className="fixed-btn scale-effect primary">
       +
     </Link>
   );
