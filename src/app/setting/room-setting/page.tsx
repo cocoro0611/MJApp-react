b1391995eb5ead034ffcf20a206f7f63df7e3ff0
Shinta Takagishi <202932018+ibm-Shinta-Takagishi@users.noreply.github.com>
2025-07-01 22:11:27 +0900

    カスタムページのルーティング調整

---
+++ src/app/setting/room-setting/page.tsx
@@ -0,0 +1,29 @@
+import Header from "@/src/components/layout/Header";
+import Main from "@/src/components/layout/Main";
+import SettingForm from "@/src/template/setting/SettingForm";
+import {
+  createSetting,
+  updateSetting,
+  readSetting,
+} from "@/src/lib/models/setting";
+
+const SettingPage = async () => {
+  const setting = await readSetting();
+
+  const formAction = setting ? updateSetting : createSetting;
+  const btnText = setting ? "更新" : "作成";
+
+  return (
+    <>
+      <Header title="ルーム設定" href="/setting" />
+      <Main>
+        <div className="bg-secondary-100 border border-secondary-500 text-secondary-800 rounded p-2 font-bold text-sm mb-8">
+          デフォルトのルーム設定を選択してください。
+        </div>
+        <SettingForm action={formAction} btnText={btnText} setting={setting} />
+      </Main>
+    </>
+  );
+};
+
+export default SettingPage;
