0e1a5361767022c571cfdd894bf22136012d6b60
cocoro <chiroru.0903.0611@icloud.com>
2025-07-05 15:14:27 +0900

    表示制御の漏れの修正

--- src/app/rooms/[roomId]/room-edit/page.tsx
+++ src/app/rooms/[roomId]/room-edit/page.tsx
@@ -2,6 +2,7 @@ import Header from "@/src/components/layout/Header";
 import Content from "@/src/components/layout/Content";
 import RoomForm from "@/src/template/rooms/RoomForm";
 import { readRoomDetail, updateRoom } from "@/src/lib/models/rooms";
+import { readSetting } from "@/src/lib/models/setting";
 
 interface RoomEditPageProps {
   params: Promise<{ roomId: string }>;
@@ -10,12 +11,18 @@ interface RoomEditPageProps {
 const RoomEditPage = async ({ params }: RoomEditPageProps) => {
   const { roomId } = await params;
   const room = await readRoomDetail(roomId);
+  const setting = await readSetting();
 
   return (
     <>
       <Header title="ルームの編集" href={`/rooms/${room.id}`} />
       <Content>
-        <RoomForm action={updateRoom} btnText="更新" room={room} />
+        <RoomForm
+          action={updateRoom}
+          btnText="更新"
+          room={room}
+          setting={setting}
+        />
       </Content>
     </>
   );
